5658093d1ee585ca04d4988ccaf948b51d3f2d38
Kirill <fedorov.kirr@gmail.com>
2025-09-28 17:49:38 +0300

    Add feature_var_2 to main.c Resolved merge conflicts

--- practical_4/main.c
+++ practical_4/main.c
@@ -3,4 +3,8 @@
 int main_var = 0;
 =======
 int feature_var_1 = 0;
+<<<<<<< HEAD
 >>>>>>> 7757747 (Add feature_var_1 to main.c)
+=======
+int feature_var_2 = 0;
+>>>>>>> 7e3ab95 (Add feature_var_2 to main.c)
